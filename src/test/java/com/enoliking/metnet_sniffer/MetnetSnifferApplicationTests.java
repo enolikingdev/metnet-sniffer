638ce135c699adbb9051f2c8db381a9014a464a8
István Kiss <istvan.kiss3@tesco.com>
2024-12-06 19:00:14 +0100

    Spring initializer

---
+++ src/test/java/com/enoliking/metnet_sniffer/MetnetSnifferApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enoliking.metnet_sniffer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetnetSnifferApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
