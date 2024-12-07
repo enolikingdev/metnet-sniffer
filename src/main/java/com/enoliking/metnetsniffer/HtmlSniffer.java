aefede3a5a2142747b3049d802625b8392fea490
István Kiss <istvan.kiss3@tesco.com>
2024-12-07 16:27:28 +0100

    Get ready for parameterized date input

--- src/main/java/com/enoliking/metnetsniffer/HtmlSniffer.java
+++ src/main/java/com/enoliking/metnetsniffer/HtmlSniffer.java
@@ -2,30 +2,38 @@ package com.enoliking.metnetsniffer;
 
 import com.enoliking.metnetsniffer.influx.Temperature;
 import com.enoliking.metnetsniffer.jsoup.DocumentProcessor;
-import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import org.jsoup.nodes.Document;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 @Component
 @RequiredArgsConstructor
 public class HtmlSniffer {
 
+    public static final String URL = "http://www.metnet.hu/online-allomasok";
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+
     private final SSLHelper sslHelper;
     private final DocumentProcessor processor;
 
     @Value("${user.agent}")
     private String userAgent;
-    private String url = "http://www.metnet.hu/online-allomasok?sub=showosdata&ostid=512&date=2024-11-30";
+    @Value("${metnet.ostid}")
+    private String ostid;
 
-    @PostConstruct
-    public List<Temperature> sniff() throws IOException {
-        Document doc = sslHelper.getConnection(url).userAgent(userAgent).get();
+    public List<Temperature> sniff(LocalDate date) throws IOException {
+        Document doc = sslHelper.getConnection(buildUrl(date)).userAgent(userAgent).get();
         return processor.getTemperature(doc);
+    }
+
+    private String buildUrl(LocalDate date) {
+        return URL + "?sub=showosdata&ostid=" + ostid + "&date=" + date.format(FORMATTER);
     }
 
 }
